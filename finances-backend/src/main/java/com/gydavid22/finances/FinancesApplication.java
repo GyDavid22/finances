a30f40f6ae9e79b625c4b2a1bf5c2e0cdece295f
David Gyenes <gyenesdavid@gmail.com>
2023-11-16 17:04:46 +0100

    Generate database creation script

--- finances-backend/src/main/java/com/gydavid22/finances/FinancesApplication.java
+++ finances-backend/src/main/java/com/gydavid22/finances/FinancesApplication.java
@@ -3,11 +3,21 @@ package com.gydavid22.finances;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import jakarta.persistence.EntityManager;
+// import jakarta.persistence.EntityManagerFactory;
+// import jakarta.persistence.Persistence;
+
 @SpringBootApplication
 public class FinancesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FinancesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FinancesApplication.class, args);
+
+        // EntityManagerFactory emf = Persistence.createEntityManagerFactory("emf");
+        // EntityManager em = emf.createEntityManager();
+
+        // em.close();
+        // emf.close();
+    }
 
 }
